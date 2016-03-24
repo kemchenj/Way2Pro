6fd53e756bf3fc692b2be6961e3775bf3b76213d
kemchenj <chenkem@gmail.com>
2016-03-25 00:15:54 +0800

    jkadsfjk

--- Algorithms/BinarySearch.java
+++ Algorithms/BinarySearch.java
@@ -1,15 +1,21 @@
 import java.util.Arrays;
+import j
+
 public class BinarySearch{
-        public static int rank(int key, int[] a){
-                int lo = 0;
-                int hi = a.length-1;
+        public static int rank(int key, int []a)
+        {  return rank(key, a, 0, a.length - 1);  }
+
+        public static int rank(int key, int[] a, int lo, int hi){
+                if (lo > hi) {
+                        return -1;
+                }
                 while (lo <= hi){
-                        int mid = lo + (hi-lo)/2;
+                        int mid = lo + (hi-lo) / 2;
                         if(key < a[mid])      hi = mid - 1;
                         else if(key < a[mid]) lo = mid + 1;
                         else                  return mid;
                 }
-                return -1;
+                return 0;
         }
 
         public static void main(String[] args) {
